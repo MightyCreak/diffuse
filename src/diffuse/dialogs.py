dc2e445c775239e9dfc903894844c78037a063b9
Romain Failliot <romain.failliot@foolstep.com>
2022-04-04 12:21:05 -0400

    Update all translation files

--- src/diffuse/dialogs.py
+++ src/diffuse/dialogs.py
@@ -46,7 +46,7 @@ class AboutDialog(Gtk.AboutDialog):
         license_text = [
             constants.APP_NAME + ' ' + constants.VERSION + '\n\n',
             constants.COPYRIGHT + '\n\n',
-            _('''This program is free software; you can redistribute it and/or modify
+            '''This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
@@ -58,7 +58,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.''')]
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.''']
         self.set_license(''.join(license_text))
 
 
