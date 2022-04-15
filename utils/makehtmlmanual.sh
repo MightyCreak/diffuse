0490de7358af04cbc95afd028b32ddc2ba6e1747
Romain Failliot <romain.failliot@foolstep.com>
2022-04-15 13:55:20 -0400

    Remove util script to generate HTML/PDF documentation

--- utils/makehtmlmanual.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xsltproc --nonet /usr/share/sgml/docbook/stylesheet/xsl/nwalsh/html/docbook.xsl ../src/usr/share/gnome/help/diffuse/C/diffuse.xml | sed 's/<\/head>/<link rel="stylesheet" href="style.css" type="text\/css"\/><\/head>/;s/<code class="email">.*<\/code>//' > diffuse.html
