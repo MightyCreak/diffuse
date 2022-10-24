9078dafca9c4842d8c1d643bea2d6d5ad9873a73
Romain Failliot <romain.failliot@foolstep.com>
2022-10-24 16:07:37 -0400

    Add test syntax files for Rust language

---
+++ tests/syntax/rust/valid_comments.rs
@@ -0,0 +1,46 @@
+pub fn comments() -> () {
+    // line comment
+
+    // line comment
+    // line comment
+
+    //
+
+    //
+
+    //
+    //
+
+    // line comment // test
+
+    /* line comment */
+
+    /* line comment */
+    /* line comment */
+
+    /* block
+       comment
+    */
+
+    /*
+       block
+       comment
+    */
+
+    /**
+       block
+       comment
+    */
+
+    /**/
+
+    /* */
+
+    /***/
+
+    // Not supported yet:
+    //
+    // Recursive block comment:
+    // - This is valid: /* line /* test */ comment */
+    // - This is invalid: /* line /* test comment */
+}
